1b86003a6efd980c0c3a538950d1c78e057650a0
0x41kravchenko <0x41kravchenko@gmail.com>
2026-01-02 12:08:57 +0100

    Revert my fixes in BleCanPloxy and clean up PIO ini

--- src/BleCanProxy.cpp
+++ src/BleCanProxy.cpp
@@ -9,31 +9,25 @@ BleCanProxy::BleCanProxy(CanDevice *candevice, Stream *stream, uint8_t vesc_id, 
 }
 
 void BleCanProxy::proxyIn(std::string in) {
-    // ESP_LOGD(LOG_TAG_BLE_CAN_PROXY, "Proxy in, input=%d\n", in.size());
-    // longPackBuffer += in;
-    // ESP_LOGD(LOG_TAG_BLE_CAN_PROXY, "Proxy in, buffer=%d\n", longPackBuffer.size());
+    ESP_LOGD(LOG_TAG_BLE_CAN_PROXY, "Proxy in, input=%d\n", in.size());
+    longPackBuffer += in;
+    ESP_LOGD(LOG_TAG_BLE_CAN_PROXY, "Proxy in, buffer=%d\n", longPackBuffer.size());
     
-    // // we need at least 4 bytes for initial packet parsing
-    // if (longPackBuffer.size() < 4) {
-    //     return;
-    // }
+    // we need at least 4 bytes for initial packet parsing
+    if (longPackBuffer.size() < 4) {
+        return;
+    }
 
-    // auto packet_type = (uint8_t) longPackBuffer.at(0);
-
-    auto packet_type = (uint8_t) in.at(0);
+    auto packet_type = (uint8_t) longPackBuffer.at(0);
 
     if (!longPacket) {
         switch (packet_type) {
             case 2:
-                // // packet_type(1) + length(1) + payload(length)) + crc(2) + end_byte(1) 
-                // length = (uint8_t) longPackBuffer.at(1);
-                // command = (uint8_t) longPackBuffer.at(2);
-                // processing = true;                
-                // if (length > 6) {
-                length = (uint8_t) in.at(1);
-                command = (uint8_t) in.at(2);
-                processing = true;
-                if (length > (in.size() - 5)) {
+                // packet_type(1) + length(1) + payload(length)) + crc(2) + end_byte(1) 
+                length = (uint8_t) longPackBuffer.at(1);
+                command = (uint8_t) longPackBuffer.at(2);
+                processing = true;                
+                if (length > 6) {
                     longPacket = true;
                 }
                 break;
@@ -50,16 +44,11 @@ void BleCanProxy::proxyIn(std::string in) {
         ESP_LOGD(LOG_TAG_BLE_CAN_PROXY, "Proxy in, command %d, length %d\n", command, length);
     }
 
-    // if (length <= 6) { // short packet (nocrc?)
+    if (length <= 6) { // short packet (nocrc?)
 
-    //     if (length + 3 > longPackBuffer.size()) {            
-    //         return;
-    if (length <= 6) {
-        Serial.printf("length <=6: in: ");
-        for (int len = 0; len < in.length(); len ++) {
-            Serial.printf("%d ", in.at(len));
+        if (length + 3 > longPackBuffer.size()) {            
+            return;
         }
-        Serial.printf("\n");
 
         twai_message_t tx_frame = {};
         tx_frame.extd = 1;
@@ -69,12 +58,10 @@ void BleCanProxy::proxyIn(std::string in) {
         tx_frame.data[1] = 0x00;
         tx_frame.data[2] = command;
         for (int i = 3; i < length + 2; i++) {
-            // tx_frame.data[i] = (uint8_t) longPackBuffer.at(i);
-            tx_frame.data[i] = (uint8_t) in.at(i);
+            tx_frame.data[i] = (uint8_t) longPackBuffer.at(i);
         }
         candevice->sendCanFrame(&tx_frame);
     } else { // long packet        
-        longPackBuffer += in;
         if (length + 5 > longPackBuffer.size()) {
             //Serial.printf("Buffer not full, needed %d, is %d\n", length + 5, longPackBuffer.size());
             return;
